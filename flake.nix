51e7d452ba0bdf3046be47e7876e2614fcc5b039
chris norris <cnorris2007@gmail.com>
2025-08-29 16:29:03 +0100

    Use flake-utils

--- flake.nix
+++ flake.nix
@@ -1,28 +1,35 @@
 {
-  inputs.nixpkgs.url = "github:nixos/nixpkgs?ref=nixpkgs-unstable";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
   outputs = {
-    self,
     nixpkgs,
+    flake-utils,
     ...
-  }: let
-    pkgs = import nixpkgs {system = "x86_64-darwin";};
-    inherit (pkgs) glibcLocales;
-    inherit (pkgs.stdenv) isLinux;
-    inherit (pkgs.lib) optionalString;
+  }:
+    flake-utils.lib.eachDefaultSystem
+    (
+      system: let
+        pkgs = import nixpkgs {system = system;};
+        inherit (pkgs) glibcLocales;
+        inherit (pkgs.stdenv) isLinux;
+        inherit (pkgs.lib) optionalString;
 
-    beamPkgs = with pkgs.beam_minimal; packagesWith interpreters.erlang_28;
-    erlang = beamPkgs.erlang;
-  in {
-    formatter.x86_64-darwin = pkgs.alejandra;
+        beamPkgs = with pkgs.beam_minimal; packagesWith interpreters.erlang_28;
+        erlang = beamPkgs.erlang;
+      in {
+        formatter = pkgs.alejandra;
 
-    devShells.x86_64-darwin.default = pkgs.mkShell {
-      packages = [erlang pkgs.nil pkgs.beam27Packages.erlang-ls pkgs.emacs30-pgtk pkgs.inetutils pkgs.agda pkgs.ccls];
+        devShells.default = pkgs.mkShell {
+          packages = [erlang pkgs.nil pkgs.beam27Packages.erlang-ls pkgs.emacs30-pgtk pkgs.inetutils pkgs.agda pkgs.ccls];
 
-      LOCALE_ARCHIVE = optionalString isLinux "${glibcLocales}/lib/locale/locale-archive";
-      LANG = "en_US.UTF-8";
+          LOCALE_ARCHIVE = optionalString isLinux "${glibcLocales}/lib/locale/locale-archive";
+          LANG = "en_US.UTF-8";
 
-      ERL_INCLUDE_PATH = "${erlang}/lib/erlang/usr/include";
-    };
-  };
+          ERL_INCLUDE_PATH = "${erlang}/lib/erlang/usr/include";
+        };
+      }
+    );
 }
